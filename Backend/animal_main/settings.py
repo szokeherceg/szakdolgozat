c79388a6865fbb560c061c04efa1940819e87c60
szokeherceg <vasas.gergo2004@gmail.com>
2025-03-16 22:56:56 +0100

    Adatbázis kapcsolat

--- Backend/animal_main/settings.py
+++ Backend/animal_main/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'animal_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'animal_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
