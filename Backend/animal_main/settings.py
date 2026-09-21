b62b914a7d7db3f313926be4a1a381fc5fa07541
szokeherceg <vasas.gergo2004@gmail.com>
2026-09-21 18:52:13 +0200

    adding allowed host

--- Backend/animal_main/settings.py
+++ Backend/animal_main/settings.py
@@ -9,7 +9,7 @@ PASSWORD = config('PASSWORD')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
